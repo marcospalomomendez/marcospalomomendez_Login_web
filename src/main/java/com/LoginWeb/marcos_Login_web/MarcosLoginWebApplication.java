1a9644ac8d6db58e43011c1f376e63b791d0797c
CAMPUSFP <marcos.palomomendez.25@campusfp.es>
2026-01-19 14:36:08 +0100

    GGGG

--- src/main/java/com/LoginWeb/marcos_Login_web/MarcosLoginWebApplication.java
+++ src/main/java/com/LoginWeb/marcos_Login_web/MarcosLoginWebApplication.java
@@ -1,13 +1,24 @@
 package com.LoginWeb.marcos_Login_web;
 
+import com.LoginWeb.marcos_Login_web.service.UsuarioService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MarcosLoginWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarcosLoginWebApplication.class, args);
-	}
 
+    public static void main(String[] args) {
+        SpringApplication.run(MarcosLoginWebApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(UsuarioService usuarioService) {
+        return args -> {
+            Menu menu = new Menu(usuarioService);
+            menu.iniciar(); // Arranca el menú interactivo
+        };
+    }
 }
